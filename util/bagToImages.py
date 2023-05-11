faad0f8d44dcaae87d69266836f83985ba23b768
rbtchun3 <chun@burro.ai>
2023-05-11 15:23:19 -0400

    refactored bagToImages.py

--- util/bagToImages.py
+++ util/bagToImages.py
@@ -4,77 +4,45 @@ import cv2
 import os
 import numpy as np
 from cv_bridge import CvBridge
+import argparse
 
 
-# Set bag file path
-bag_file = "/home/chun/Downloads/20230216_130118_archive/20230216_130118_archive_extracted.bag"
+def main(topics_to_extract, bag_path):
+    # Set bag file path
+    # "/home/chun/Downloads/20230216_130118_archive/20230216_130118_archive_extracted.bag"
+    # "./*.bag"
+    bag_directory = bag_path[0:bag_path.rindex('/')]
 
-# Set output directory path
-r_output_dir = "/home/chun/Downloads/20230216_130118_archive/R"
-l_output_dir = "/home/chun/Downloads/20230216_130118_archive/L"
+    output_paths = {}
 
-rl_output_dir = "/home/chun/Downloads/20230216_130118_archive/RL"
+    # Set output directory path
+    # Create output directory if it doesn't exist
+    for topic_ in topics_to_extract:
+        output_paths[topic_] = os.path.join(bag_directory, topic_.replace('/', '-'))
+        if not os.path.exists(output_paths[topic_]):
+            os.makedirs(output_paths[topic_])
 
-r_raw_output_dir = "/home/chun/Downloads/20230216_130118_archive/Rraw"
-l_raw_output_dir = "/home/chun/Downloads/20230216_130118_archive/Lraw"
+    # Open bag file
+    bag = rosbag.Bag(bag_path)
+    bridge = CvBridge()
+    # Loop through bag file and extract images
+    for topic, msg, t in bag.read_messages(topics = topics_to_extract):
+        # Extract image from message
+        cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
-# Create output directory if it doesn't exist
-if not os.path.exists(r_output_dir):
-    os.makedirs(r_output_dir)
+        # Construct filename for image
+        filename = "image_{}.jpg".format(t.to_nsec())
+        cv2.imwrite(os.path.join(output_paths[topic], filename), cv_img)
 
-if not os.path.exists(l_output_dir):
-    os.makedirs(l_output_dir)
+        # Save image to output directory
 
-if not os.path.exists(rl_output_dir):
-    os.makedirs(rl_output_dir)
+    # Close bag file
+    bag.close()
 
-if not os.path.exists(r_raw_output_dir):
-    os.makedirs(r_raw_output_dir)
-
-if not os.path.exists(l_raw_output_dir):
-    os.makedirs(l_raw_output_dir)
-
-# Open bag file
-bag = rosbag.Bag(bag_file)
-l_ct = r_ct = 0
-bridge = CvBridge()
-# Loop through bag file and extract images
-for topic, msg, t in bag.read_messages(topics=["/camera5/infra1/image_rect", "/camera5/infra2/image_rect", "/camera1/infra1/image_rect_raw", "/camera1/infra2/image_rect_raw", "/camera5/infra1/image_rect_raw", "/camera5/infra2/image_rect_raw"]):
-    # Extract image from message
-    cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-
-    # Construct filename for image
-    if 'camera1/infra1' in topic:
-        # left
-        filename = "image_{}_l.jpg".format(t.to_nsec())
-        cv2.imwrite(os.path.join(rl_output_dir, filename), cv_img)
-        continue
-    elif 'camera1/infra2' in topic:
-        # right
-        filename = "image_{}_r.jpg".format(t.to_nsec())
-        cv2.imwrite(os.path.join(rl_output_dir, filename), cv_img)
-        continue
-
-    if topic == '/camera5/infra1/image_rect':
-        # left
-        filename = "image_{}_l.jpg".format(t.to_nsec())
-        cv2.imwrite(os.path.join(l_output_dir, filename), cv_img)
-    elif topic == '/camera5/infra2/image_rect':
-        # right
-        filename = "image_{}_r.jpg".format(t.to_nsec())
-        cv2.imwrite(os.path.join(r_output_dir, filename), cv_img)
-
-    # if topic == '/camera5/infra1/image_rect_raw':
-    #     # left
-    #     filename = "image_{}_l.jpg".format(t.to_nsec())
-    #     cv2.imwrite(os.path.join(l_raw_output_dir, filename), cv_img)
-    # elif topic == '/camera5/infra2/image_rect_raw':
-    #     # right
-    #     filename = "image_{}_r.jpg".format(t.to_nsec())
-    #     cv2.imwrite(os.path.join(r_raw_output_dir, filename), cv_img)
-
-
-    # Save image to output directory
-
-# Close bag file
-bag.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='extract odom and measure distance')
+    parser.add_argument('-b', '--bag_path', type=str, nargs=1, required=True,
+                        help='path to the config file')
+    parser.add_argument('-t', '--topics', nargs='+', default=[])
+    args = parser.parse_args()
+    main(args.topics, args.bag_path)
